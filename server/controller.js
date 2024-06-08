7196dbb28ec7631b862fd4254d3f33458f3992a6
Abubakar Shahid <abubakarshahid1309@gmail.com>
2024-06-08 19:25:37 +0500

    some changes in getUserInfo

--- server/controller.js
+++ server/controller.js
@@ -26,15 +26,19 @@ export async function getUserInfo(req, res) {
     try {
         const [userInfo] = await pool.query(`SELECT * FROM users where id = ?`, [req.body.id]);
         const [ballInfo] = await pool.query(`SELECT * FROM ballinformation where username = ?`, [userInfo[0].username]);
-        const resData = {
-            message: "User Found!",
-            username: userInfo[0].username,
-            password: userInfo[0].password,
-            x: ballInfo[0].x_coordinate,
-            y: ballInfo[0].y_coordinate,
-            z: ballInfo[0].z_coordinate
-        };
-        res.status(200).json(resData);
+        if (userInfo.length === 0) {
+            res.status(404).json({ message: 'User not found' });
+        } else {
+            const resData = {
+                message: "User Found!",
+                username: userInfo[0].username,
+                password: userInfo[0].password,
+                x: ballInfo[0].x_coordinate,
+                y: ballInfo[0].y_coordinate,
+                z: ballInfo[0].z_coordinate
+            };
+            res.status(200).json(resData);
+        }
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Internal server error' });
@@ -42,5 +46,10 @@ export async function getUserInfo(req, res) {
 }
 
 export async function saveState(req, res) {
-    
+    try {
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
 }
