9449a990ccd7be979ea9cae607bc716f1a05397c
Abubakar Shahid <abubakarshahid1309@gmail.com>
2024-06-08 19:11:06 +0500

    createUser completed

--- server/controller.js
+++ server/controller.js
@@ -1,7 +1,25 @@
 import pool from './database.js';
 
 export async function createUser(req, res) {
-    
+    try {
+        const [dbResponse1] = await pool.query(
+            `INSERT INTO users (username, email, password) VALUES (?, ?, ?)`,
+            [req.body.username, req.body.email, req.body.password]
+        );
+        const [dbResponse2] = await pool.query(
+            `INSERT INTO ballinformation (username, x_coordinate, y_coordinate, z_coordinate)
+            VALUES (?, ?, ?, ?)`,
+            [req.body.username, '0', '0', '0']
+        );
+        const resData = {
+            message: "Data Inserted Successfully!",
+            id: dbResponse1.insertId
+        };
+        res.json(resData);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
 }
 
 export async function getUser(req, res) {
